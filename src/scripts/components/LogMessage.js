8e27aeac8020c823d9e8d809836f4ed8c4c562a5
Ikilonchic <43388441+Ikilonchic@users.noreply.github.com>
2021-02-23 20:40:28 +0200

    Refactor LogMessage component

--- src/scripts/components/LogMessage.js
+++ src/scripts/components/LogMessage.js
@@ -12,28 +12,43 @@ class LogMessage {
     this.rootElement = rootElement;
     this.rootElement.classList.add('log-message');
 
-    this.message = '';
-    this.type = 'default';
+    this.messageList = [];
 
     this.show = this.show.bind(this);
     this.hide = this.hide.bind(this);
+    this.addMessage = this.addMessage.bind(this);
     this.render = this.render.bind(this);
     this.buildView = this.buildView.bind(this);
+    this.registerListener = this.registerListener.bind(this);
+
+    this.registerListener();
   }
 
   show(message, type = 'error', time = 2000) {
-    this.message = message;
-    this.type = type;
-
-    this.render();
+    this.addMessage(message, type, time);
 
     this.rootElement.classList.add('log-message_show');
-
-    setTimeout(this.hide, time);
+    this.render();
   }
 
   hide() {
     this.rootElement.classList.remove('log-message_show');
+  }
+
+  addMessage(message, type = 'error', time = 2000) {
+    const id = (this.messageList[this.messageList.length - 1]?.id + 1) || 0;
+
+    this.messageList = this.messageList.slice(-4);
+    this.messageList.push({
+      id,
+      message,
+      type: messageTypes.includes(type) ? type : 'default',
+      timeoutId: setTimeout(() => {
+        this.messageList = this.messageList.filter((value) => value.id !== id);
+        !this.messageList.length && this.hide();
+        this.render();
+      }, time),
+    });
   }
 
   render() {
@@ -41,12 +56,18 @@ class LogMessage {
   }
 
   buildView() {
-    return `<div class="log-message__text-wrap log-message_type-${messageTypes.includes(this.type) ? this.type : 'default'}">
-              <div class="log-message__text"><div>${this.message}</div></div>
-            </div>`;
+    return this.messageList.reduce((prev, next) => {
+      return prev += `<div class="log-message__text-wrap log-message_type-${next.type}">
+                        <div class="log-message__text"><div>${next.message}</div></div>
+                      </div>`;
+    }, '');
+  }
+
+  registerListener() {
+    this.rootElement.addEventListener('click', this.hide);
   }
 }
 
 export default new LogMessage({
   rootElement: document.getElementById('log-message'),
-})
+});
