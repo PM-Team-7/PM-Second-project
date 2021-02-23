098d7e5f29859baab9edb4879718a266cabec943
Daryna Kupchyk <dasha.kupchik28@gmail.com>
2021-02-23 08:55:21 +0200

    Moved classes to component folder

--- src/scripts/models/User.js
+++
@@ -1,9 +0,0 @@
-export default class User {
-  static get token() {
-    return window.localStorage.getItem('token');
-  }
-
-  static set token(value) {
-    return window.localStorage.setItem('token', value);
-  }
-}
